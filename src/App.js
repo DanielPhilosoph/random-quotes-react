7ebabab2f2b79cd48db94bdb6031a8808af9c45e
DanielPhilosoph <danielph9@gmail.com>
2021-11-18 11:43:08 +0200

    Done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import QuoteBox from "./core/QuoteBox";
+
+function App() {
+  return (
+    <div className="App">
+      <QuoteBox />
+    </div>
+  );
+}
+
+export default App;
